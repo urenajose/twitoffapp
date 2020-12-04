325f9748e885b3f5ee7d28f07905383cb3d102b2
Alexander Kim <>
2020-12-03 23:12:57 -0500

    updates

--- src/models.py
+++ src/models.py
@@ -5,28 +5,16 @@ DB = SQLAlchemy()
 class User(DB.Model):
 	id = DB.Column(DB.Integer, primary_key=True)
 	name = DB.Column(DB.String(30), nullable=False)
+	def __repr__(self):
+		return f'<User: {self.name}>'
+
 
 class Tweet(DB.Model):
 	id = DB.Column(DB.Integer, primary_key=True)
 	text = DB.Column(DB.Unicode(280), nullable=False)
+	embedding = DB.Column(DB.PickleType, nullable=False)
 	user_id = DB.Column(DB.Integer, DB.ForeignKey('user.id'), nullable=False)
 	user = DB.relationship('User', backref=DB.backref('tweets', lazy=True))
 
 	def __repr__(self):
 		return f'<Tweet: {self.text}>'
-
-
-# flask shell
-# >>> from src.models import DB, User, Tweet
-# >>> DB.create_all()
-# >>> user1 = User(name='elonmusk')
-# >>> user2 = User(name='billgates')
-# >>> tweet1 = Tweet(text='What if Mars was like Earth?')
-# >>> tweet2 = Tweet(text='coding is not difficult')
-# >>> user1.tweets.append(tweet1)
-# >>> user1.tweets.append(tweet2)
-# >>> tweet3 = Tweet(text='Windows is amazing!!!')
-# >>> user2.tweets.append(tweet3)
-# >>> DB.session.add(user1)
-# >>> DB.session.add(user2)
-# >>> DB.session.commit()
